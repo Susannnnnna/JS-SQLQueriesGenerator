fea4f2273f2348fc9b3027ebd899825fddefef50
Susannnnnna <123886691+Susannnnnna@users.noreply.github.com>
2024-02-22 10:56:38 +0100

    Update sample-data-filling.js

--- js/sample-data-filling.js
+++ js/sample-data-filling.js
@@ -1,7 +1,7 @@
 function fillFormWithSampleDataSingleRow() {
     document.getElementById("tableName").value = "TableExampleName";
-    document.getElementById("columnNames").value = "ColumnName1,ColumnName2,ColumnName3,ColumnName4";
-    document.getElementById("dataValues").value = "Value1,Value2,Value3,Value4";
+    document.getElementById("columnNames").value = "ColumnName1,ColumnName2";
+    document.getElementById("dataValues").value = "Value1,Value2";
     document.getElementById("primaryKey").value = "exampleId";
     document.getElementById("primaryKeyValue").value = "1";
 }
